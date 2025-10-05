d501a69becd4eed222592922c3160c27cc9f31e4
ravjot07 <ravu2004@gmail.com>
2025-10-06 01:57:18 +0530

    minor changes
    Signed-off-by: ravjot07 <ravu2004@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const { connect } = require("./mongo");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
 
